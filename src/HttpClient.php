2cbf66cef4ce6d60ad13c8246c37fbd4076b0b7b
SignDocs Brasil <administrativo@signdocs.com.br>
2026-07-30 07:13:17 -0300

    Release v1.9.0
    Bump version 1.8.0 -> 1.9.0 across composer.json, HttpClient SDK_VERSION,
    and CHANGELOG for the envelopes->cancel() addition.

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -19,7 +19,7 @@ use SignDocsBrasil\Api\Errors\TimeoutException;
  */
 class HttpClient
 {
-    private const SDK_VERSION = '1.8.0';
+    private const SDK_VERSION = '1.9.0';
 
     private readonly GuzzleClient $guzzle;
     private readonly RetryHandler $retry;
