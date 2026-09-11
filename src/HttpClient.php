06c04d111ded9203c6248530412dc69484698792
SignDocsBrasil <administrativo@signdocs.com.br>
2026-09-11 07:45:22 -0300

    Release 2.1.0

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -19,7 +19,7 @@ use SignDocsBrasil\Api\Errors\TimeoutException;
  */
 class HttpClient
 {
-    private const SDK_VERSION = '2.0.1';
+    private const SDK_VERSION = '2.1.0';
 
     private readonly GuzzleClient $guzzle;
     private readonly RetryHandler $retry;
